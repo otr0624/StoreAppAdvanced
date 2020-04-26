8411bccb41f046f9d82be1dc70b170ce8b328d63
otr0624 <otr0624@mail.com>
2020-04-26 19:28:55 -0400

    Added manual user confirmation and feedback website (Lecture 33)

--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, make_response, render_template
 from flask_restful import Resource
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import (
@@ -97,4 +97,5 @@ class UserConfirm(Resource):
             return {'message': USER_NOT_FOUND}, 404
         user.activated = True
         user.save_to_db()
-        return {'message': USER_CONFIRMED}, 200
+        headers = {"Content-Type": "text/html"}
+        return make_response(render_template("confirmation_page.html", email=user.username), 200, headers)
