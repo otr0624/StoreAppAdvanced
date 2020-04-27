0713376fcd7a0b5c8fd7c59864063db9d395a722
otr0624 <otr0624@mail.com>
2020-04-27 14:37:43 -0400

    Refactored Mailgun functions into separate library (Lecture 37)

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -1,13 +1,23 @@
-def send_confirmation_email(self) -> Response:
-    link = request.url_root[:-1] + url_for("userconfirm", user_id=self.id)
+from typing import List
+from requests import Response, post
 
-    return post(
-        f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-        auth=("api", MAILGUN_API_KEY),
-        data={
-            "from": f"{FROM_TITLE} <{FROM_EMAIL}>",
-            "to": self.email,
-            "subject": "Registration confirmation",
-            "text": f"Please click the link to confirm your registration: {link}",
-        },
-    )
+class Mailgun:
+    MAILGUN_DOMAIN = "sandboxec53074368814fa0ba8c880269cac21e.mailgun.org"
+    MAILGUN_API_KEY = "e1b75e8d80341924884aa05ec6c68d1d-f135b0f1-3a521536"
+
+    FROM_TITLE = "Stores REST API"
+    FROM_EMAIL = "mailgun@sandboxec53074368814fa0ba8c880269cac21e.mailgun.org"
+
+    @classmethod
+    def send_email(cls, email: List[str], subject: str, text: str, html: str) -> Response:
+        return post(
+            f"https://api.mailgun.net/v3/{cls.MAILGUN_DOMAIN}/messages",
+            auth=("api", cls.MAILGUN_API_KEY),
+            data={
+                "from": f"{cls.FROM_TITLE} <{cls.FROM_EMAIL}>",
+                "to": email,
+                "subject": subject,
+                "text": text,
+                "html": html,
+            },
+        )
--- models/user.py
+++ models/user.py
@@ -1,11 +1,7 @@
 from flask import request, url_for
 from db import db
-from requests import Response, post
-
-MAILGUN_DOMAIN = "sandboxec53074368814fa0ba8c880269cac21e.mailgun.org"
-MAILGUN_API_KEY = "e1b75e8d80341924884aa05ec6c68d1d-f135b0f1-3a521536"
-FROM_TITLE = "Stores REST API"
-FROM_EMAIL = "mailgun@sandboxec53074368814fa0ba8c880269cac21e.mailgun.org"
+from requests import Response
+from libs.mailgun import Mailgun
 
 class UserModel(db.Model):
     __tablename__ = "users"
@@ -26,17 +22,12 @@ class UserModel(db.Model):
 
     def send_confirmation_email(self) -> Response:
         link = request.url_root[:-1] + url_for("userconfirm", user_id=self.id)
+        subject = "Registration confirmation",
+        text = f"Please click the link to confirm your registration: {link}",
+        html = f'<html>Please click the link to confirm your registration: <a href="{link}">{link}</a></html>',
 
-        return post(
-            f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-            auth=("api", MAILGUN_API_KEY),
-            data={
-                "from": f"{FROM_TITLE} <{FROM_EMAIL}>",
-                "to": self.email,
-                "subject": "Registration confirmation",
-                "text": f"Please click the link to confirm your registration: {link}",
-            },
-        )
+        return Mailgun.send_email([self.email], subject, text, html)
+
 
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
