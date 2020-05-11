555bbe31f4b94ca91d022f820e45591b2332c2e8
otr0624 <otr0624@mail.com>
2020-05-11 16:51:31 -0400

    Created image upload resource and associated strings

---
+++ resources/image.py
@@ -0,0 +1,26 @@
+from flask_restful import Resource
+from flask_uploads import UploadNotAllowed
+from flask import request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from libs import image_helper
+from libs.strings import gettext
+from schemas.image import ImageSchema
+
+image_schema = ImageSchema()
+
+
+class ImageUpload(Resource):
+    @jwt_required
+    def post(self):
+        """Used to upload an image file, save image to user's folder"""
+        data = image_schema.load(request.files)
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"  # e.g., static/images/user_1
+        try:
+            image_path = image_helper.save_image(data["image"], folder=folder)
+            basename = image_helper.get_basename(image_path)
+            return {"message": gettext("image_uploaded").format(basename)}, 201
+        except UploadNotAllowed:
+            extension = image_helper.get_extension(data["image"])
+            return {"message": gettext("image_illegal_extension").format(extension)}, 400
