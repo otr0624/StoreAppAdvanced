743b260fe0da85939e8281650879a8165635ec94
otr0624 <otr0624@mail.com>
2020-05-13 11:03:28 -0400

    Added user avatar function (Lecture 83)

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefre
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.confirmation import Confirmation, ConfirmationByUser
-from resources.image import ImageUpload, Image
+from resources.image import ImageUpload, Image, AvatarUpload
 from libs.image_helper import IMAGE_SET
 
 from ma import ma
@@ -110,6 +110,7 @@ api.add_resource(Confirmation, "/user_confirm/<string:confirmation_id>")
 api.add_resource(ConfirmationByUser, "/confirmation/user/<int:user_id>")
 api.add_resource(ImageUpload, "/upload/image")
 api.add_resource(Image, "/image/<string:filename>")
+api.add_resource(AvatarUpload, "/upload/avatar")
 
 if __name__ == "__main__":
     db.init_app(app)
--- schemas/user.py
+++ schemas/user.py
@@ -1,9 +1,9 @@
 from ma import ma
-from marshmallow import pre_dump, Schema, fields
+from marshmallow import pre_dump, fields
 from models.user import UserModel
 
 
-class UserSchema(Schema):
+class UserSchema(ma.SQLAlchemyAutoSchema):
     id = fields.Int()
     username = fields.Str(required=True)
     password = fields.Str(required=True)
@@ -13,6 +13,7 @@ class UserSchema(Schema):
         model = UserModel
         load_only = ("password",)
         dump_only = ("id", "confirmation")
+        load_instance = True
 
     @pre_dump
     def _pre_dump(self, user: UserModel):
