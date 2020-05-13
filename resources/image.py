2b042e0c0918572926108abbfcc81604d6b9fc7a
otr0624 <otr0624@mail.com>
2020-05-13 10:33:19 -0400

    Added image retrieval and deletion (Lecture 82)

--- resources/image.py
+++ resources/image.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource
 from flask_uploads import UploadNotAllowed
-from flask import request
+from flask import send_file, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
+import traceback
+import os
 
 from libs import image_helper
 from libs.strings import gettext
@@ -24,3 +26,20 @@ class ImageUpload(Resource):
         except UploadNotAllowed:
             extension = image_helper.get_extension(data["image"])
             return {"message": gettext("image_illegal_extension").format(extension)}, 400
+
+
+class Image(Resource):
+    @jwt_required
+    def get(self, filename: str):
+        """Returns requested image if it exists within logged in user's folder"""
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"
+        if not image_helper.is_filename_safe(filename):
+            return {'message': gettext("image_illegal_filename").format(filename)}, 400
+        try:
+            return send_file(image_helper.get_path(filename, folder=folder))
+        except FileNotFoundError:
+            return {'message': gettext("image_not_found").format(filename)}, 404
+
+    def delete(self, filename: str):
+        pass
